2ce082d15ff6ddf2a610f2658dfb6e019bcf9d63
kamil-kurzaj <kamil.kurzaj04@gmail.com>
2025-10-03 11:28:20 +0200

    1

---
+++ Rozmowa-Rekrutacyjna/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Rozmowa_Rekrutacyjna;
+
+public class StarPattern
+{
+
+// *
+// **
+// ***
+// ****
+// *****
+    public void RightTriangleStar()
+    {
+        for (int i = 0; i <5; i++)
+        {
+            for (int j = 0 ; j< i+1; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    public void InvertedRightTriangleStar()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = i; j < 5; j++)
+            {
+                Console.Write("*");
+                
+            }
+            Console.WriteLine("");
+        }
+        
+    }
+
+
+
+    public class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+           StarPattern starPattern= new StarPattern();
+           starPattern.InvertedRightTriangleStar();
+           
+        }
+        
+    }
+    
+    
+}
